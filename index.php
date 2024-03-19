0df48a5dbb09dd3de38005bbe3f17bc9702ed997
mattia_morriale <mattia.morriale1@gmail.com>
2024-03-19 16:35:48 +0100

    milestone 1 completata

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$number = $_GET['number'];
+
+function passGenerate($number) {
+
+    $pass = '';
+    
+    for($i = 0; $i < $number; $i++){
+
+        $pass .= chr(rand(33,126));
+
+    };
+
+    return $pass;
+
+}
+
+?>
+
+<!-- _______________________________________________________ -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +37,32 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body data-bs-theme='dark'>
+
+    <div class="container p-5">
+        <h1 class='mb-5'>Generatore di Password</h1>
+        <hr>
+        <form>
+            <div class="my-5">
+                <label for="examplepass" class="form-label">Numero dei Caratteri</label>
+                <input type="number" class="form-control" id="examplepass" name='number' aria-describedby="emailHelp">
+                <div id="passHelp" class="form-text">Inserisci il numero di caratteri, con cui la tua password verrà generata.</div>
+            </div>
+            <button type="submit" class="btn btn-primary">Genera</button>
+        </form>
+        <hr>
+        <label for="examplepass" class="form-label">Password Generata</label>
+        <div class='form-control my-pass d-flex justify-content-center align-items-center'>
+            <p class='m-0'><?php
+            
+            echo passGenerate($number);
+            
+            ?></p>
+        </div>
+    </div>
+
+
+
+
 
     <!-- bootscript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
